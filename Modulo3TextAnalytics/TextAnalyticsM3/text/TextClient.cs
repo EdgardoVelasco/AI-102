ba195324ccbc76a3b42550664c65aac0890c3f84
edgardo.nvf <edgardo.nvf@gmail.com>
2022-03-01 11:57:49 -0600

    Add M3

---
+++ Modulo3TextAnalytics/TextAnalyticsM3/text/TextClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Azure.AI.TextAnalytics;
+using Azure;
+
+namespace TextAnalyticsM3.text
+{
+    class TextClient
+    {
+        private static readonly string API_KEY = "02efc28c161949efb8872cb5aef84120";
+        private static readonly string ENDPOINT = "https://cognitivenvfne.cognitiveservices.azure.com/";
+        public static TextAnalyticsClient text { get; private set; }
+
+        static TextClient() { InitClient(); }
+
+        private static void InitClient() {
+            if (text == null) {
+                var credentials = new AzureKeyCredential(API_KEY);
+                text = new TextAnalyticsClient(new Uri(ENDPOINT), credentials);
+            }
+        }
+    }
+}
