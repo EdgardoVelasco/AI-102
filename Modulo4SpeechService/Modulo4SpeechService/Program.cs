ab40c9c1ce05618b3224c97fc2eb0fa9ca3f3df5
edgardo.nvf <edgardo.nvf@gmail.com>
2022-03-01 13:04:13 -0600

    Proyecto Speech C#

---
+++ Modulo4SpeechService/Modulo4SpeechService/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+namespace Modulo4SpeechService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var servicio = speech.SpeechClient.Speech;
+            Console.WriteLine("Habla en el micrófono");
+            reconociendoVoz(servicio);
+            Console.ReadKey();
+
+        }
+
+        private static async void reconociendoVoz(SpeechConfig speech) {
+            var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            var speechRecogn = new SpeechRecognizer(speech, "es-MX", audioConfig);
+            var respuesta = await speechRecogn.RecognizeOnceAsync();
+            Console.WriteLine($"Texto reconocido: {respuesta.Text}");
+        }
+    }
+}
---
+++ Modulo4SpeechService/Modulo4SpeechService/speech/SpeechClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CognitiveServices.Speech;
+
+namespace Modulo4SpeechService.speech
+{
+    /*Esta clase se usa para conectarnos al servicio
+     * de Speech en azure
+    */
+    class SpeechClient
+    {
+        /*APIKEY y ENDPOINT lo extraigo del portal de Azure*/
+        private static readonly string APIKEY = "4a3caeeed30b4120a631a25ec63158c0";
+        private static readonly string ENDPOINT = "https://eastus.api.cognitive.microsoft.com/sts/v1.0/issuetoken";
+
+        /*Variable necesaria para usar el servicio
+         en cualquier parte de mi proyecto*/
+
+        public static SpeechConfig Speech { get; private set; }
+
+        /*Constructor usado para inicializar nuestro servicio de speech*/
+        static SpeechClient() { Init(); }
+
+        private static void Init() {
+            if (Speech==null) {
+                Speech = SpeechConfig.FromEndpoint(new Uri(ENDPOINT), APIKEY);
+            }
+        }
+
+    }
+}
